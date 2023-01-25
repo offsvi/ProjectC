b1605916ab5ab20c46cfb0a03af7c682c5b90c41
Vitor Oliveira <vitorsk807@gmail.com>
2023-01-25 20:52:11 -0300

    Finalizando exercicio 3

---
+++ exe3.cs
@@ -0,0 +1,16 @@
+var n1 = 5;
+var n2 = 9;
+var n3 = 10;
+
+if (n1 < n2) {
+    Console.WriteLine("Condição 1 é satisfeita!!");
+    Console.WriteLine("Resultado da soma é: {0}", n1 + n2);
+}
+if (n2 < n3) 
+{
+    Console.WriteLine(n3 + n2)
+}
+else
+{
+    Console.WriteLine("Condição 2 não é satisfeita!!.");
+}
